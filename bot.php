49cd64be20b8e5eb170dca0195d8287e9f7269df
Nocivobeats <136031361+Nocivobeats@users.noreply.github.com>
2023-06-18 09:56:30 -0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 
-// Bot criado por @DARKNETSSH código original para o @DARKNETSSHBOT
+// Bot criado por @NocivoBeatsSshFree_bot código original para o @NocivoBeatsSshFree_bot
 
 date_default_timezone_set ('America/Sao_Paulo'); // define timestamp padrão
 
@@ -47,7 +47,7 @@ switch ($tlg->Text ()){
 		'text' => $textoMsg->start,
 		'parse_mode' => 'html',
 		'reply_markup' => $tlg->buildInlineKeyBoard ([
-			[$tlg->buildInlineKeyboardButton ('🇧🇷 SSH Gratis BR 🇧🇷', null, '/sshgratis')]
+			[$tlg->buildInlineKeyboardButton ('🇧🇷 CONTA GRATIS 🇧🇷', null, '/sshgratis')]
 		])
 	]);
 
@@ -56,7 +56,7 @@ switch ($tlg->Text ()){
 
 	$tlg->sendMessage ([
 		'chat_id' => $tlg->ChatID (),
-		'text' => 'Bot original @DARKNETSSHBOT por @DARKNETSSH'
+		'text' => 'Bot original @NocivoBeatsSshFree_bot por @NocivoBeats'
 	]);
 
 	break;
@@ -90,7 +90,7 @@ switch ($tlg->Text ()){
 
 		exec ('./gerarusuario.sh '.$usuario.' '.$senha.' 1 1');
 
-		$textoSSH="🇧🇷 Conta SSH criada ;)\r\n\r\n<b>Servidor:</b> <code>".$ip."</code>\r\n<b>Usuario:</b> <code>".$usuario."</code>\r\n<b>Senha:</b> <code>".$senha."</code>\r\n<b>Logins:</b> 1\r\n<b>Validade:</b> ".date ('d/m', strtotime('+1 day'))."\r\n\r\n Oferecido por @NocivoBeats";
+		$textoSSH="  CONTA CRIADA ;)\r\n\r\n<b>Servidor:</b> <code>".$ip."</code>\r\n<b>Usuario:</b> <code>".$usuario."</code>\r\n<b>Senha:</b> <code>".$senha."</code>\r\n<b>Logins:</b> 1\r\n<b>Validade:</b> ".date ('d/m', strtotime('+1 day'))."\r\n\r\n Oferecido por @NocivoBeats";
 
 		$redis->setex ($tlg->UserID (), 43200, 'true'); //define registro para ser guardado por 12h
 
